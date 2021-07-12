85812504018b8eab86884bbc0b13a196c9efcf2a
alishadnaidu <alishanaidu@fb.com>
2021-07-12 15:55:47 -0700

    Basic fragment layout completed

--- app/src/main/java/com/example/candlebox/MainActivity.java
+++ app/src/main/java/com/example/candlebox/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.candlebox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.tabs.TabLayout;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     //inflate actionbar
