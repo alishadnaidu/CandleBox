f2adf02a32960f583f8bbdaf7d6e34519fb4c93e
alishadnaidu <alishanaidu@fb.com>
2021-07-12 16:43:47 -0700

    Log layout skeleton created

--- app/src/main/java/com/example/candlebox/AddActivity.java
+++ app/src/main/java/com/example/candlebox/AddActivity.java
@@ -1,31 +1,21 @@
 package com.example.candlebox;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.material.tabs.TabLayout;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.parse.ParseUser;
 
-public class MainActivity extends AppCompatActivity {
-
+public class AddActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Get the ViewPager and set it's PagerAdapter so that it can display items
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
-                MainActivity.this));
-
-        // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        setContentView(R.layout.activity_add);
     }
 
     //inflate actionbar
@@ -44,13 +34,13 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.logout) {
             ParseUser.logOut();
             ParseUser currentUser = ParseUser.getCurrentUser();
-            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            Intent i = new Intent(AddActivity.this, LoginActivity.class);
             startActivity(i);
             finish();
             return true;
         }
-        if (item.getItemId() == R.id.add) {
-            Intent i = new Intent(MainActivity.this, AddActivity.class);
+        if (item.getItemId() == R.id.home) {
+            Intent i = new Intent(AddActivity.this, MainActivity.class);
             startActivity(i);
             return true;
         }
