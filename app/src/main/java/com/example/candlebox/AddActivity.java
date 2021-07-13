8a480d005aa852c4c14c51b4829753755529d5a1
alishadnaidu <alishanaidu@fb.com>
2021-07-13 10:47:54 -0700

    User can log hours, data shows up in back4app database

--- app/src/main/java/com/example/candlebox/AddActivity.java
+++ app/src/main/java/com/example/candlebox/AddActivity.java
@@ -3,25 +3,69 @@ package com.example.candlebox;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class AddActivity extends AppCompatActivity {
 
+    public static final String TAG = "AddActivity";
     private EditText etHours;
+    private Button btnLog;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
         etHours = findViewById(R.id.etHours);
+        btnLog = findViewById(R.id.btnLog);
 
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //assign the integer value with etHours to hours integer variable
+                Integer hours = Integer.parseInt(etHours.getText().toString());
+                //if user has not inputted anything, show a toast
+                if (hours.toString().isEmpty() || hours == 0) {
+                    Toast.makeText(AddActivity.this, "Hours cannot be empty or 0!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                saveStats(hours, currentUser);
+                Intent i = new Intent(AddActivity.this, MainActivity.class);
+            }
+        });
+    }
+
+    //saves stats (hours and user), uploads to back4app database
+    private void saveStats(Integer hours, ParseUser currentUser) {
+        Stats stats = new Stats();
+        stats.setHours(hours);
+        stats.setUser(currentUser);
+        stats.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "error while saving stats");
+                    Toast.makeText(AddActivity.this, "Error while saving stats", Toast.LENGTH_SHORT).show();
+                }
+                Log.i(TAG, "Stats save was successful!");
+                Toast.makeText(AddActivity.this, "Successfully logged hours!", Toast.LENGTH_SHORT).show();
+                etHours.setText("");
+            }
+        });
     }
 
     //inflate actionbar
--- app/src/main/java/com/example/candlebox/MainActivity.java
+++ app/src/main/java/com/example/candlebox/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
