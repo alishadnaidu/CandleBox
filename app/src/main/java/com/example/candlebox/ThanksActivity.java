489a13b3f230caf8cee2a0790ba5b752ad4a75d7
alishadnaidu <alishanaidu@fb.com>
2021-07-16 12:19:00 -0700

    User is taken to thank you page upon uploading candle

---
+++ app/src/main/java/com/example/candlebox/ThanksActivity.java
@@ -0,0 +1,60 @@
+package com.example.candlebox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseUser;
+
+public class ThanksActivity extends AppCompatActivity {
+
+    //private ImageView ivThanks;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanks);
+
+        //ivThanks.findViewById(R.id.ivThanks);
+        //ivThanks.setBackgroundResource();
+    }
+
+    //inflate actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // comes into play when an item in the actionbar is clicked
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // if the logout icon is tapped, log out + navigate to the login screen
+        if (item.getItemId() == R.id.logout) {
+            ParseUser.logOut();
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            Intent i = new Intent(ThanksActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+            return true;
+        }
+        if (item.getItemId() == R.id.home) {
+            Intent i = new Intent(ThanksActivity.this, MainActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (item.getItemId() == R.id.scan) {
+            Intent i = new Intent(ThanksActivity.this, BarcodeScannerActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
